44ddef6b47b77718f451e0e7888d70598e34669b
Nirmala Kumari <knskumari@gmail.com>
2016-06-20 16:17:19 +0530

    yhjmnyuj

---
+++ Sample_GitHub/src/classes/SampleClass.java
@@ -0,0 +1,10 @@
+package classes;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sample_GitHub project");
+	}
+
+}
